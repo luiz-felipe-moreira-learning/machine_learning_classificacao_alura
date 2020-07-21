490ebbae009b468e84aca2c1111532ccbb734617
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-07-21 09:59:09 -0300

    aula 2

---
+++ dados.py
@@ -0,0 +1,19 @@
+import csv
+
+#le arquivo csv com os dados de acesso coletados
+def carregar_acessos():
+
+    X = []
+    Y = []
+
+    arquivo = open("../acesso.csv", 'rb')
+    leitor = csv.reader(arquivo)
+
+    #joga fora primeira linha descritiva
+    leitor.next()
+
+    for home, como_funciona, contato, comprou in leitor:
+        dado = [int(home), int(como_funciona), int(contato)]
+        X.append(dado)
+        Y.append(int(comprou))
+    return X, Y
